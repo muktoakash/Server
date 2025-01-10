eda672fe812357f74df00646ce74c22efe3bb0b1
Mukto Akash <muktoakash@gmail.com>
2025-01-09 23:55:18 -0500

    Remove all files for trial 2

--- src/utils/pages.config.js
+++
@@ -1,8 +0,0 @@
-// src/utils/pages.config.js
-
-// Add pages to convert here:
-module.exports = [
-  { template: 'index.ejs', output: 'index.html', data: { title: "Home" } },
-  { template: 'pages/page-2.ejs', output: 'page-2/index.html', data: { title: "Page 2" } },
-  { template: 'pages/page-3.ejs', output: 'page-3/index.html', data: { title: "Page 3" } }
-];
